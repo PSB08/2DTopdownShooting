f9435bf918cffdf735569c52ce3709bed621428e
PSB08 <Parkseongbin08@gmail.com>
2025-07-06 21:45:37 +0900

    feat : EntityState

---
+++ Assets/A.Work/01.Scripts/Entities/EntityAnimatorTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Scripts.Entities
+{
+    public class EntityAnimatorTrigger : MonoBehaviour, IEntityComponent
+    {
+        public Action OnAnimationEndTrigger;
+        
+        private Entity _entity;
+
+        public void Initialize(Entity entity)
+        {
+            _entity = entity;
+        }
+        
+        private void AnimationEnd()
+        {
+            OnAnimationEndTrigger?.Invoke();
+        }
+    }
+}
