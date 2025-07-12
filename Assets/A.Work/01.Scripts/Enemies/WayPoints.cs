2cfbe7a82559bfbab0b382ff67e36d1c521d4f44
PSB08 <Parkseongbin08@gmail.com>
2025-07-12 21:54:48 +0900

    feat : Way point

---
+++ Assets/A.Work/01.Scripts/Enemies/WayPoints.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Code.Scripts.Enemies
+{
+    public class WayPoints : MonoBehaviour
+    {
+        [SerializeField] private WayPoint[] wayPoints;
+        
+        private int _currIndex = -1;
+
+        public Vector3 GetNextWayPoint()
+        {
+            Debug.Assert(wayPoints.Length >= 1, "1개 이상의 waypoint 필요");
+            _currIndex = (_currIndex + 1) % wayPoints.Length;
+            return wayPoints[_currIndex].Position;
+        }
+        
+    }
+}
