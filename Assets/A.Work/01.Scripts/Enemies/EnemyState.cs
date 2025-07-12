12a253faaea77332e849334fc61c5fe733a1af9b
PSB08 <Parkseongbin08@gmail.com>
2025-07-13 00:10:07 +0900

    feat : Enemy state

---
+++ Assets/A.Work/01.Scripts/Enemies/EnemyState.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Behavior;
+
+namespace Code.Scripts.Enemies
+{
+    [BlackboardEnum]
+    public enum EnemyState
+    {
+        PATROL = 1,
+        CHASE = 2,
+        ATTACK = 3
+    }
+}
