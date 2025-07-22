d73d0e9fc23dc80c3f426982f07e727b59ae962a
PSB08 <Parkseongbin08@gmail.com>
2025-07-22 23:10:23 +0900

    feat : Map size up down

---
+++ Assets/A.Work/01.Scripts/Core/MapManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Scripts.Core
+{
+    public class MapManager : MonoBehaviour
+    {
+        [SerializeField] private RectTransform map;
+
+        private bool _isBig = false;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.M) && !_isBig)
+            {
+                _isBig = true;
+                map.localScale = new Vector3(2f, 2f, 2f);
+                map.anchoredPosition = new Vector3(-300, 300, 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.M) && _isBig)
+            {
+                _isBig = false;
+                map.localScale = new Vector3(1, 1, 1);
+                map.anchoredPosition = new Vector3(-150, 150, 0);
+            }
+        }
+        
+        
+    }
+}
