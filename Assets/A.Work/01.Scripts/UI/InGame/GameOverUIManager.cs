b41684c2a266d5b451db3d633d80c7bce58c45aa
PSB08 <Parkseongbin08@gmail.com>
2025-07-22 16:45:09 +0900

    feat : SPUM UI

---
+++ Assets/A.Work/01.Scripts/UI/InGame/GameOverUIManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Code.Scripts.UI.InGame
+{
+    public class GameOverUIManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject gameOverUI;
+
+        private void Awake()
+        {
+            Time.timeScale = 1;
+            gameOverUI.SetActive(false);
+        }
+
+        public void GameOver()
+        {
+            Time.timeScale = 0;
+            gameOverUI.SetActive(true);
+        }
+        
+    }
+}
